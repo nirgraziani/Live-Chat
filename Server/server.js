1ec19bce5efd17d11ef3a3c152189baf4d40ffa2
Nir Graziani <nirg6888@gmail.com>
2021-12-21 15:12:29 +0200

    finished basiv server with socket.io

---
+++ Server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+const app = express();
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server is running");
+});
